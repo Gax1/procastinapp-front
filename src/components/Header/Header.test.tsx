d6b5668630c9383dc748537c86102bd0f54cd59e
Gabriel Andres <gabrielleoandres@gmail.com>
2022-09-05 13:17:28 +0200

    Added test for corner case render in Header test

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,6 +1,14 @@
 import { render, screen } from "@testing-library/react";
+import { useAppSelector } from "../../store/hooks/hooks";
 import { Wrapper } from "../../test-utils/Wrapper/Wrapper";
 import { Header } from "./Header";
+
+jest.mock("react-redux", () => ({
+  ...jest.requireActual("react-redux"),
+  useAppSelector: jest.fn().mockReturnValue({
+    isUserLoggedIn: true,
+  }),
+}));
 
 describe("Given a header component", () => {
   describe("When rendered", () => {
