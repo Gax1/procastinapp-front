b444fdc2e56f8f86af94a3322ab46c0ea55761be
Gabriel Andres <gabrielleoandres@gmail.com>
2022-09-08 12:16:14 +0200

    Added task card and style

---
+++ src/components/Task/TaskStyled.ts
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+
+const TaskStyled = styled.section`
+  background-color: ${(props) => props.theme.colors.secondaryColor};
+  border-radius: 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  .task-card-header {
+    display: flex;
+    flex-direction: row;
+    gap: 20px;
+    align-items: center;
+    margin-top: 20px;
+  }
+  .task-description {
+    width: 85%;
+  }
+  .circle {
+    height: 35px;
+    width: 35px;
+    border-radius: 50%;
+    border: 17.5px solid;
+  }
+  .circle__very {
+    border-color: red;
+  }
+  .circle__medium {
+    border-color: orange;
+  }
+  .circle__small {
+    border-color: green;
+  }
+  .icons-footer {
+    font-size: 30px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    width: 100%;
+    margin-bottom: 20px;
+  }
+`;
+
+export default TaskStyled;
