ad47be339d7d0d31d866494ef67699c4ce4f68f1
Gabriel Andres <gabrielleoandres@gmail.com>
2022-09-08 12:37:39 +0200

    Added test to task component

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,6 @@ import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import AuthLogin from "./components/AuthComponent/AuthLoginComponent";
 import { Loading } from "./components/Loading/Loading";
-import Task from "./components/Task/Task";
-import { Task as Itask } from "./interfaces/interfaces";
-
 import { DayTasksPage } from "./pages/DayTasksPage/DayTasksPage";
 import { LoginPage } from "./pages/LoginPage/LoginPage";
 import { NotFoundPage } from "./pages/NotFoundPage/NotFoundPage";
@@ -12,24 +9,13 @@ import { RegisterPage } from "./pages/RegisterPage/RegisterPage";
 import { useAppSelector } from "./store/hooks/hooks";
 import { RootState } from "./store/store";
 
-const tasks: Itask = {
-  date: "22/07/2022",
-  description:
-    "Description of the task extended for propose of seing a long description",
-  id: "id12345",
-  img: "img",
-  importance: "medium",
-  owner: "test-id",
-  title: "Title of the task",
-};
-
 function App() {
   const { isLoadding } = useAppSelector(
     (state: RootState) => state.ui.notification
   );
   return (
     <>
-      {/* <div className="main-container">
+      <div className="main-container">
         {isLoadding && <Loading />}
         <Routes>
           <Route path="/" element={<Navigate to={"/login"} />} />
@@ -41,8 +27,7 @@ function App() {
           />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
-      </div> */}
-      <Task tasks={tasks} />
+      </div>
     </>
   );
 }
---
+++ src/components/Task/Task.test.tsx
@@ -0,0 +1,35 @@
+import { render, screen } from "@testing-library/react";
+import { Task as Itask } from "../../interfaces/interfaces";
+import { Wrapper } from "../../test-utils/Wrapper/Wrapper";
+import Task from "./Task";
+
+const tasks: Itask = {
+  date: "22/07/2022",
+  description:
+    "Description of the task extended for propose of seing a long description",
+  id: "id12345",
+  img: "img",
+  importance: "very",
+  owner: "test-id",
+  title: "Title of the task",
+};
+
+describe("Given a Task component", () => {
+  describe("When its rendered with a task", () => {
+    test("Then it should show a heading and paragraph", () => {
+      render(
+        <Wrapper>
+          <Task tasks={tasks} />
+        </Wrapper>
+      );
+
+      const title = screen.getByRole("heading", { name: "Title of the task" });
+      const paragraph = screen.getByText(
+        "Description of the task extended for propose of seing a long description"
+      );
+
+      expect(title).toBeInTheDocument();
+      expect(paragraph).toBeInTheDocument();
+    });
+  });
+});
