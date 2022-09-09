4546c4729c67725ebb43260a3e0e1ad87e34e8c3
Gabriel Andres <gabrielleoandres@gmail.com>
2022-09-09 18:55:59 +0200

    Added test for task repository

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -40,7 +40,7 @@ export const handlers = [
       message: "Error-test",
     };
 
-    const id = await req.url.searchParams.get("id");
+    const id: string | null = await req.url.searchParams.get("id");
     const date = await req.url.searchParams.get("date");
 
     const status = id === "" ? 404 : 200;
@@ -48,5 +48,28 @@ export const handlers = [
     response = date === "" ? [] : task;
 
     return res(ctx.status(status), ctx.json({ tasks: response }));
+  }),
+  rest.post(`${apiUrl}/tasks/my-day`, async (req, res, ctx) => {
+    const newtask = {
+      date: "09/05/2022",
+      description: "test-description",
+      id: "test-id",
+      img: "test-img",
+      importance: "very",
+      owner: "test-idOwner",
+      title: "title-test",
+    };
+    const error = {
+      message: "Error",
+    };
+
+    const request: any = await req;
+
+    const usernameData = request._body.get("username");
+
+    const status = usernameData === null ? 400 : 201;
+    const response = usernameData === null ? error : newtask;
+
+    return res(ctx.status(status), ctx.json({ task: response }));
   }),
 ];
