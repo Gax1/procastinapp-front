ae3de629de127d177c664e9aec71f9c48ccfb200
Gabriel Andres <gabrielleoandres@gmail.com>
2022-09-13 20:17:58 +0200

    Added test for editTask render in taskForm test

--- src/components/TaskForm/TaskForm.test.tsx
+++ src/components/TaskForm/TaskForm.test.tsx
@@ -15,7 +15,6 @@ describe("Given a taskForm component", () => {
   const titlePlaceHolder = "here goes the title...";
   const dateText = "Pick a Date:";
   const describePlaceHolder = "Write here youre description...";
-  const buttonText = "Create Task";
   const importanceText = "How importan is this task for you?";
   const initalFormState: InitialForm = {
     title: "",
@@ -27,6 +26,7 @@ describe("Given a taskForm component", () => {
   const id = "test-id";
 
   describe("When rendered", () => {
+    const buttonText = "Create Task";
     test("Then it should show a button and inputs", () => {
       const {
         result: {
@@ -66,17 +66,15 @@ describe("Given a taskForm component", () => {
     const testIdForm = "taskForm";
     const onSubmitMock = jest.fn();
     const imgMock = new File([""], "");
+    const buttonText = "Create Task";
 
     test("Then it should call the onSubmit function", async () => {
-      const initalTask: Task = {
+      const initalTask = {
         title: "",
         description: "",
         date: "",
         importance: "",
         img: "test-img",
-        id: "",
-        owner: "test-owner",
-        backUpImg: "",
       };
 
       const {
@@ -115,6 +113,66 @@ describe("Given a taskForm component", () => {
       });
       await fireEvent.change(importance, { target: { value: "very" } });
       await userEvent.upload(selectFile, imgMock);
+      await userEvent.click(button);
+
+      await waitFor(() => {
+        expect(onSubmitMock).toHaveBeenCalled();
+      });
+    });
+  });
+  describe("When it receives an edit function", () => {
+    jest.clearAllMocks();
+    const titlePlaceHolder = "here goes the title...";
+    const testIdForm = "taskForm";
+    const onSubmitMock = jest.fn();
+    const buttonText = "Update";
+    test("Then it should call the submit function", async () => {
+      const initalTask: Task = {
+        title: "",
+        description: "",
+        date: "",
+        importance: "",
+        img: "test-img",
+        id: "",
+        owner: "test-owner",
+        backUpImg: "",
+      };
+
+      const {
+        result: {
+          current: { editTask },
+        },
+      } = renderHook(() => useTasks(), { wrapper: Wrapper });
+      render(
+        <Wrapper>
+          <TaskForm
+            buttonText="Update"
+            navigation="/my-day"
+            sendData={editTask}
+            initialData={initalTask}
+            id={id}
+          />
+        </Wrapper>
+      );
+
+      const formTask = screen.getByTestId(testIdForm);
+      formTask.onsubmit = onSubmitMock;
+
+      const titleInput = screen.getByPlaceholderText(titlePlaceHolder);
+      const textAreaDescription =
+        screen.getByPlaceholderText(describePlaceHolder);
+
+      const importance = screen.getByLabelText(importanceText);
+      // const selectFile = screen.getByTestId("img");
+      const button = screen.getByRole("button", {
+        name: buttonText,
+      });
+
+      await fireEvent.change(titleInput, { target: { value: "test-title" } });
+      await fireEvent.change(textAreaDescription, {
+        target: { value: "test-decription" },
+      });
+      await fireEvent.change(importance, { target: { value: "very" } });
       await userEvent.click(button);
 
       await waitFor(() => {
